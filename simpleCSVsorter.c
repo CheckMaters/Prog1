4aceca6dfc0a7d512abcd923067dcdd8f5761ffa
CheckMaters <39863034+CheckMaters@users.noreply.github.com>
2018-10-21 14:05:02 -0400

    Update simpleCSVsorter.c

--- simpleCSVsorter.c
+++ simpleCSVsorter.c
@@ -17,8 +17,8 @@
 //lksakjflkdjflk
 int main(int argc, char* argv[]) {
 	//checking if the input argument is correct or Not
-	
-	
+
+
 	//the output file is not given in the parameter
 	if(argc == 5 || argc == 7) {
 		if(strcmp("-c", argv[1]) != 0 || strcmp("-d", argv[3]) != 0) {
@@ -31,27 +31,29 @@ int main(int argc, char* argv[]) {
 					return -1;		//returning -1 because it's an error
 				}
 		}
-	} 
+	}
 	else {
 		printf("Parameters are not correctly formated\n");
 		return -1;  			//returning -1 because it's an error
 	}
-	
+
 	/*
 	this will create a directory and also
 	a struct that keeps all the information
 	about directory.
 	*/
-	DIR * directory = opendir(argv[4]);
-	char * main_path;
-	strcpy(main_path, argv[4]);
+
+	DIR * directory;
+	directory = opendir(argv[4]);
+
 		if(directory == NULL) {
 			printf("ERROR! Given directory is pointing to NULL\n");
 			return -1;		//returning -1 because it's an error
 		}
 
 		char* sort_By_This_Value = argv[2];
-		return scan_Directory(directory, sort_By_This_Value, main_path);
+
+		return scan_Directory(directory, sort_By_This_Value, argv[4]);
 
 }
 
@@ -66,14 +68,14 @@ by sort_By_This_Value.
 int sort_The_List(char* sort_By_This_Value, FILE* file) {
 
 	// Buffer for reading from stdin
-	BUFFER read_Stdin_File;																	// Buffer is created to read the stdin File
+	BUFFER read_File;																	// Buffer is created to read the stdin File
 	BUFFER header_Of_File;																// For holding the header line
 
 
 	FUNCTION_POINTER_CMPDATA pFuncCompare = compare;
 	//Initializing Buffer to NULL and then will add data to it
-	read_Stdin_File.data = NULL;
-	read_Stdin_File.length_Of_Data = 0;
+	read_File.data = NULL;
+	read_File.length_Of_Data = 0;
 
 	int output_Result = 1;			//setting the output result to 1 in the beginning
 
@@ -97,28 +99,28 @@ int sort_The_List(char* sort_By_This_Value, FILE* file) {
 	movie_List.iSize = 0;
 	int no_Use = 0;
 		// Read the header and get the index for the sorting key
-		if (getline(&(read_Stdin_File.data), &(read_Stdin_File.length_Of_Data), file) > 0 ) {
-			delete_Newline_Char_At_The_End(&read_Stdin_File);						// this will delete the new line character from the end of the string line
-			//
-			header_Of_File.data = (char*)malloc (sizeof(char) * read_Stdin_File.length_Of_Data);
-			header_Of_File.length_Of_Data = read_Stdin_File.length_Of_Data;
+		if (getline(&(read_File.data), &(read_File.length_Of_Data), file) > 0 ) {
+			delete_Newline_Char_At_The_End(&read_File);						// this will delete the new line character from the end of the string line
+
+			header_Of_File.data = (char*)malloc (sizeof(char) * read_File.length_Of_Data);
+			header_Of_File.length_Of_Data = read_File.length_Of_Data;
 
 
 
 
-	//		initialize_Buffer(&header_Of_File, read_Stdin_File.length_Of_Data);			//Holds a line from the file
-			strcpy(header_Of_File.data, read_Stdin_File.data);										// copy of header line before tokenizing
-			//
 
-			if (get_Column_Index_To_Sort_List(sort_By_This_Value, &column_Info, read_Stdin_File.data)) {
+			strcpy(header_Of_File.data, read_File.data);			// copy of header line before tokenizing
+			//head_Of_File keeps record of the first line that is that name of all the columns
+
+			if (get_Column_Index_To_Sort_List(sort_By_This_Value, &column_Info, read_File.data)) {
 							printf( "Error! Unable to parse the input column name\n");
 						}
 						else {
 				// Read data line by line. Creates record and put them into array
-				while (getline(&(read_Stdin_File.data), &(read_Stdin_File.length_Of_Data), stdin) > 0) {
-					delete_Newline_Char_At_The_End(&read_Stdin_File);
+				while (getline(&(read_File.data), &(read_File.length_Of_Data), file) > 0) {
+					delete_Newline_Char_At_The_End(&read_File);
 
-					one_Movie_Values* pRecord = make_Movie_Value_List(read_Stdin_File.data, &column_Info);
+					one_Movie_Values* pRecord = make_Movie_Value_List(read_File.data, &column_Info);
 					if (pRecord) {
 					no_Use =	add_One_Movie_To_The_List(&movie_List, pRecord);
 					}
@@ -133,6 +135,7 @@ int sort_The_List(char* sort_By_This_Value, FILE* file) {
 				*/
 				mergeSort((void**)movie_List.pRecArray, 0, movie_List.iSize - 1, &column_Info, pFuncCompare);
 				// Output sorted records to file
+				printf("sorted\n");
 				print_The_List(&header_Of_File, &movie_List);
 				 output_Result = 0;
 		}
@@ -142,7 +145,7 @@ int sort_The_List(char* sort_By_This_Value, FILE* file) {
 		}
 
 	freeBuffer(&header_Of_File);
-	freeBuffer(&read_Stdin_File);
+	freeBuffer(&read_File);
 	if (movie_List.pRecArray != NULL) {
 		clearMovieList(&movie_List);
 	}
@@ -175,36 +178,41 @@ int is_CSV_File (const char * name) {
 
 
 /*
-this function will scan through the directory 
-to find .csv files and possibly other 
+this function will scan through the directory
+to find .csv files and possibly other
 sub-directories if there's any.
-It also performs sorting on finding 
-CSV file with sorting_Column, which is the 
+It also performs sorting on finding
+CSV file with sorting_Column, which is the
 column user wants to sort file on.
 */
 int scan_Directory(DIR * directory, char * sorting_Column, char * path){
+
 	if(directory == NULL){
 		printf("ERROR! Unable to open directory. NULL pointer\n");
 		return -1;			//returning -1 because it is an error
 	}
-	
-	//setting this value to unusal so that if later program breaks, we can check if this value changes or not
-	int return_Value = -500;	
-	int PID = -500;		// this will store the PID
+
+	//setting this value to an unusal value so that if later program breaks, we can check if this value changes or not
+	int return_Value = -500;
+	int PID = -500;		// this PID will store the PID of child going through sub-directory
+	int PID_Two = -500;	//this PID will store the PID of child sorting csv file
 	struct dirent * directory_Info;
-	char * current_dir_path;
+	char current_dir_path[strlen(path) + 1];
+
 	strcpy(current_dir_path, path);
-	
+
 	while ((directory_Info = readdir(directory))!= NULL){
-		
-		
-		//this if statement will skip over the first . and .. in the directory
-		if((strcpy(directory_Info->d_name, ".") == 0) || (strcpy(directory_Info->d_name, "..") == 0)){
+
+printf("This is the file name part one: %s\n", directory_Info->d_name);
+
+		//this if statement will skip over the first . and .. in the directory. d_name is the name of current file/pointer
+		if((strcmp(directory_Info->d_name, ".") == 0) || (strcmp(directory_Info->d_name, "..") == 0)){
 			continue;
 		}
-		
+
+printf("This is the file name: %s\n", directory_Info->d_name);
 		/*
-		concatenating path as each file within directory 
+		concatenating path as each file within directory
 		will have different path.
 		e.g-> directory/file1
 			directory/file2
@@ -212,45 +220,55 @@ int scan_Directory(DIR * directory, char * sorting_Column, char * path){
 		*/
 		path = strcat(path, "/");
 		path = strcat(path, directory_Info->d_name);
-		
-		
+
+
 		//checks if the current file that file pointer points to is a directory or not
 		return_Value = is_Directory(path);
 		/*
 		if return value is zero, it means this is directory
-		and will perform recurssion from this point on that 
+		and will perform recurssion from this point on that
 		sub-directory
 		*/
 		if(return_Value == 0) {
 			//**************this means it is directory, perform recurssion*****************
 			PID = fork();
 			int status = 0;		// this is just got parents to wait until child returns
-			
-			
+
+
 			if(PID == 0) {			//this means it's a child Process
 				strcpy(current_dir_path, path);	//changing the current path for child process as it is going to be at sub directory level
 				directory = opendir(path);	//changing directory to sub directory for child process
 			}
 			else if(PID > 0) {
-				wait(&status);	//parent waiting until child returns
+				wait(&status);	//parent waiting until child returns going through sub level directory;
 			}
 		}
-		
-		
+
+
 		// if the read value is not directory, check if it's CSV file or not
 		else {
 			return_Value = is_CSV_File(path);
 			if(return_Value == 0){
 				//************this means it is CSV file, perform sorting******************
-				FILE * file = fopen(path, "r");
-				return_Value = sort_The_List(sorting_Column, file);
-			} 
+				PID = fork();
+				int status_Two = 0;
+
+				if(PID == 0) {
+					FILE * file = fopen(path, "r");
+					return_Value = sort_The_List(sorting_Column, file);
+					return return_Value;
+				}
+				else if (PID > 0) {
+					wait(&status_Two);	//parent waiting until child sorts the CSV file.
+				}
+			}
 			else {
 				printf("%s is not a csv file or directory\n", path);
+				strcpy(path, current_dir_path);
 				continue; //continuing to the next loop because read value isn't directory or CSV file
 			}
 		}
-	strcpy(path,current_dir_path);	//changing directory/file1 to directory, because we are at same level but just looping to next file	
+	strcpy(path,current_dir_path);	//changing directory/file1 to directory, because we are at same level but just looping to next file
 	}
 	return return_Value;
 }
